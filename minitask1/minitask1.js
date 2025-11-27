65cc95e6308d9e26a9e78f2bab4297d2bbf7f01e
Rahman <fadhllulrahman687@gmail.com>
2025-11-27 15:21:45 +0700

    feat[minitask1]: add dom program or logic to make eye icon can open/close

---
+++ minitask1/minitask1.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  const passwordInput = document.getElementById("password");
+  const togglePassword = document.getElementById("toggle-password");
+  const eyeOpen = document.getElementById("eye-open");
+  const eyeClosed = document.getElementById("eye-closed");
+
+  togglePassword.addEventListener("click", function () {
+    let type;
+    if (passwordInput.getAttribute("type") === "password") {
+      type = "text";
+    } else {
+      type = "password";
+    }
+    passwordInput.setAttribute("type", type);
+
+    eyeOpen.classList.toggle("hidden");
+    eyeClosed.classList.toggle("hidden");
+  });
+});
